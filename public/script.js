e9cb9d59fd1eccce700e835b940241aa4f5d7161
nariIng <168060650+nariIng@users.noreply.github.com>
2024-09-30 12:57:23 +0000

    commited

--- public/script.js
+++ public/script.js
@@ -1,7 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
   const sensorTableBody = document.querySelector('#sensorTable tbody');
-  const downloadBtn = document.getElementById('downloadBtn');
-  const resetBtn = document.getElementById('resetBtn');
 
   // Fonction pour récupérer les données et mettre à jour le tableau
   function fetchData() {
@@ -32,22 +30,7 @@ document.addEventListener('DOMContentLoaded', function() {
       .catch(error => console.error('Error fetching data:', error));
   }
 
-  // Fonction pour télécharger les données en Excel
-  downloadBtn.addEventListener('click', function() {
-    window.location.href = '/api/download-excel';
-  });
-
-  // Fonction pour réinitialiser la base de données
-  resetBtn.addEventListener('click', function() {
-    fetch('/api/reset-data', { method: 'POST' })
-      .then(response => response.json())
-      .then(message => {
-        alert(message.message);
-        fetchData(); // Actualiser le tableau
-      })
-      .catch(error => console.error('Error resetting data:', error));
-  });
-
   // Appeler fetchData toutes les 5 secondes pour mettre à jour les données en direct
   setInterval(fetchData, 5000);
 });
+
