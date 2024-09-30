19ecb3551c2fbe993dc82e33742b9f0f8cae61b8
nariIng <168060650+nariIng@users.noreply.github.com>
2024-09-30 11:58:25 +0000

    test4

--- public/script.js
+++ public/script.js
@@ -30,3 +30,35 @@ document.addEventListener('DOMContentLoaded', function() {
   // Appeler fetchData toutes les 5 secondes pour mettre à jour les données en direct
   setInterval(fetchData, 5000);
 });
+
+const hamburger = document.querySelector("#toggle-btn");
+
+hamburger.addEventListener("click", function(){
+    document.querySelector("#sidebar").classList.toggle("expand");
+})
+
+function downloadExcel() {
+  const worksheet = XLSX.utils.json_to_sheet(data, { header: ["time", "humidity", "temperature", "temperature_1" ,"temperature_2", 
+                                                              "temperature_3", "temperature_4", "temperature_5"] });
+  const workbook = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(workbook, worksheet, "Sensor Data");
+
+  // Définir les noms des colonnes
+  XLSX.utils.sheet_add_aoa(worksheet, [["Temperature_1 (°C)", "Temperature_2 (°C)", "Temperature_3 (°C)",
+                                        "Temperature_4 (°C)", "Temperature_5 (°C)"]], { origin: "A1" });
+
+  const excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+  const blob = new Blob([excelBuffer], { type: 'application/octet-stream' });
+
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(blob);
+  link.download = "sensor_data.xlsx";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+
+function resetTable() {
+  data = [];
+  updateTable();
+}
