71bb95276b1b5c52e349ef723aea08b0626a9647
nariIng <168060650+nariIng@users.noreply.github.com>
2024-10-04 06:41:29 +0000

    height

--- public/script.js
+++ public/script.js
@@ -66,7 +66,12 @@ document.addEventListener('DOMContentLoaded', function() {
             `;
   
             sensorTableBody.appendChild(row);
-            
+            document.getElementById('time').textContent = entry.time;
+            document.getElementById('temperature_1').textContent = entry.T_1;
+            document.getElementById('temperature_2').textContent = entry.T_2;
+            document.getElementById('temperature_3').textContent = entry.T_3;
+            document.getElementById('temperature_4').textContent = entry.T_4;
+            document.getElementById('temperature_5').textContent = entry.T_5;
           });
   
           // Mise à jour du graphique avec toutes les données
